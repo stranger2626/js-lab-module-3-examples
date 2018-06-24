a2497f8611ec13dd6b99dff7c97ee214f62100b5
Матвей Мурашко <stranger2626@gmail.com>
2018-06-24 21:45:14 +0300

    added usage examples of string methods

---
+++ string_operations_examples.js
@@ -0,0 +1,50 @@
+const arrayOfNumbers = [ 5 ,8, 11, 9, 3, 27];
+const personalInfromationObject = {
+    name: "Mathew",
+    age: 24,
+    birthDate: 'May 16 1994',
+    favoriteNumbers: arrayOfNumbers
+};
+const age = 24;
+const name = 'Mathew';
+const birthDate = 'May 16 1994'
+//How to generate a string which contains values from variables
+
+//Basic string concatination using the "+"" sign
+const oldWayString = 'Hello, my name is ' + name + '. I was borne on ' + birthDate + '. And now I am ' + age + ' years old.'; // this will be equal "Hello, my name is Mathew. I was borne on May 16 1994. And now I am 24 years old."
+
+//A more modern way to print out the same string using string templating from es6
+const templatingString = `Hello, my name is ${name}. I was borne on ${birthDate}. And now I am ${age} years old.`; // this will also be equal "Hello, my name is Mathew. I was borne on May 16 1994. And now I am 24 years old."
+
+//You can combine separate strings using string.concat() method
+const concatinatedString = name.concat(' is ', age, ' years old.'); // this will be qual "Mathew is 24 years old."
+
+//You can reference variables, functions and objects inside strings, though the referenced elements will be converted to string 
+const stringWithInfrormationFromAnObject = `Mathew is ${personalInfromationObject.age} years old.`; // this will be equal "Mathew is 24 years old."
+
+//You can reference elements from an array using []
+const stringWithANumberFromAnArray = `Mathew's favorite number is ${arrayOfNumbers[0]}.`; //this will equal "Mathew's favorite number is 5."
+const stringWithANumberFromAnArrayFromAnObject = `Mathew's favorite number is ${personalInfromationObject.favoriteNumbers[0]}.`; //this will also equal "Mathew's favorite number is 5."
+
+
+
+
+//Method often used with strings
+
+//indexOf()
+// it will return the position of the element you hav passed to it. If the element if absent from this string it will return -1
+const yearPosition = birthDate.indexOf('1994'); //this will equal 7
+const mLetterPosition = birthDate.indexOf('M'); //this will equal 0
+const noneExistingLetterPossition = birthDate.indexOf('I do not exist'); //this will return -1 as it is supposed to :)
+
+//trim()
+// this method removes spece\tabulation simbols from the start of a stering and from its end
+const stringWithSpaces = '      I have a lot of spaces.     ';
+const stringWithoutSpaces = stringWithSpaces.trim(); //this will equal 'I have a lot of spaces.'
+// console.log(`|${stringWithSpaces}|`); //this is here just for the sake of providing an example. Uncomment these two lines to see for yourself 
+// console.log(`|${stringWithoutSpaces}|`); //this is here just for the sake of providing an example. Uncomment these two lines to see for yourself 
+
+//split()
+// this method returns an array of strings that were separated by the string passed to this method as an argument
+const stringOfMonths = 'Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec';
+const arrayOfMonths = stringOfMonths.split(','); //this will be equal [ 'Jan',' Feb',' Mar',' Apr',' May',' Jun',' Jul',' Aug',' Sep',' Oct',' Nov',' Dec' ]
