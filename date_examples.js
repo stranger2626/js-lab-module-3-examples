d3b638677aa8a4f21b5c53cce218a6de75299969
Матвей Мурашко <stranger2626@gmail.com>
2018-06-25 18:21:42 +0300

    added a simple example of creating a Date object

---
+++ date_examples.js
@@ -0,0 +1,6 @@
+//creating a Date object
+
+const birthday = new Date('May 16 1994');
+const birthday2 = new Date('16 May 1994');
+const birthday3 = new Date('05/16/1994');
+// all of the above Date objects will be the same
